46c48fde635565eafa49436f217c089a20d1dd58
Justin B. Van Horne <justinvh@gmail.com>
2014-02-23 20:14:43 -0700

    Add clone support for projects
    - Add new upload-pack dispatch method
    - Add ssh_user for utils pertaining to authenticated users

---
+++ python/llab/auth/ssh_user.py
@@ -0,0 +1,47 @@
+import os
+
+from llab.web.project.models import Project
+from llab.web.account.models import User
+
+
+class AuthenticationError(Exception):
+    """The user failed to authenticate"""
+
+
+class BadUserError(AuthenticationError):
+    """The user is invalid"""
+
+
+class BadProjectError(AuthenticationError):
+    """The project is invalid"""
+
+
+def authenticate(user_id, project_path):
+    try:
+        # Verify that this project is managed and available
+        path_split = project_path.split(os.sep)
+        if len(path_split) != 2:
+            raise BadProjectError('An invalid project was specified.')
+
+        owner, project_name = path_split
+        project = Project.objects.get(owner__username=owner, name=project_name)
+        user = User.objects.get(pk=user_id)
+
+        # Easy check, owners can always commit
+        if project.owner == user:
+            return project, user
+
+        # If they are part of the organization and have commit access
+        if project.organization and project.organization.user_can_commit(user):
+            return project, user
+
+        # Otherwise the user can not commit to this project.
+        m = '[DENIED] {} does not have permissions to commit to "{}".'
+        raise AuthenticationError(m.format(user.username, project.full_name()))
+
+    except Project.DoesNotExist as e:
+        raise BadProjectError('[DENIED] An invalid project was specified.')
+
+    except User.DoesNotExist as e:
+        raise BadUserError('[DENIED] You are not registered.')
+
